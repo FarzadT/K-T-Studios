6150ef8778e8cb1d5de388d881cedd52041370b6
Mohamad Kouli <mohamad.kouli@gmail.com>
2011-09-25 20:59:54 -0400

    Added the whole project to git

---
+++ myServer/src/server/servlet/myServer.java
@@ -0,0 +1,62 @@
+package server.servlet;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandler.Context;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+
+public class myServer 
+{
+	private static String HOST = "localhost";
+	private static int PORT = 8080;
+	
+	private static int TPOOL_MIN = 5;
+	private static int TPOOL_MAX = 50;
+	
+	private static Server _server;
+	private static AbstractHandler _contexts;
+	
+	public myServer(String pHost, int pPort, 
+					int pMinTPool, int pMaxTPool)
+	{
+		HOST = pHost;
+		PORT = pPort;
+		
+		TPOOL_MIN = pMinTPool;
+		TPOOL_MAX = pMaxTPool;
+		
+		init();
+	}
+	
+	public myServer()
+	{
+		init();
+	}
+	
+	private void init()
+	{
+		_server = new Server();
+		
+		SelectChannelConnector connector = new SelectChannelConnector();
+		connector.setHost(HOST);
+		connector.setPort(PORT);
+		
+		_server.addConnector(connector);
+		
+		_contexts = new ContextHandlerCollection();
+	}
+		
+	public void start()
+	{
+		
+	}
+	/*
+	 * FOR TESTING/DEBUGGING
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		
+	}
+}
