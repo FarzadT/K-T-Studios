9053f7625f2e081d76beb07c85cd7574106d869f
Mohamad Kouli <mohamad.kouli@gmail.com>
2011-10-02 16:18:01 -0400

    Added all of the resource handlers to serve files in all of the root folders.

--- myServer/src/main/Main.java
+++ myServer/src/main/Main.java
@@ -1,9 +1,21 @@
 package main;
 
+import server.myServer;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		myServer server = new myServer();
 		
+		try 
+		{
+			server.start();
+		} 
+		
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- myServer/src/server/myServer.java
+++ myServer/src/server/myServer.java
@@ -1,22 +1,22 @@
 package server;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ContextHandler.Context;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class myServer 
 {
 	private static String HOST = "localhost";
-	private static int PORT = 8080;
+	private static int PORT = 9090;
 	
 	private static int TPOOL_MIN = 5;
 	private static int TPOOL_MAX = 50;
 	
 	private static Server _server;
-	private static AbstractHandler _contexts;
+	private static ContextHandlerCollection _contexts;
 	
 	public myServer(String pHost, int pPort, 
 					int pMinTPool, int pMaxTPool)
@@ -46,11 +46,64 @@ public class myServer
 		_server.addConnector(connector);
 		
 		_contexts = new ContextHandlerCollection();
+		_server.setHandler(_contexts);
+		
+		QueuedThreadPool qtp = new QueuedThreadPool();
+		qtp.setMinThreads(TPOOL_MIN);
+		qtp.setMaxThreads(TPOOL_MAX);
+		
+		_server.setThreadPool(qtp);
+		
+		initContexts();
 	}
 		
-	public void start()
+	private void initContexts() 
 	{
+		initResourceHandler();	
+	}
+
+	/**
+	 * Method used to initialize all of the resource handlers
+	 */
+	private void initResourceHandler() 
+	{
+		//Add the HTML resource handler
+		addResourceHandler("./site/html/", "/html", true);
 		
+		//Add the CSS Resource Handler
+		addResourceHandler("./site/css/", "/css", true);
+		
+		//Add the images resource handler
+		addResourceHandler("./site/images/", "/images", true);
+		
+		//Add the javascript resource handler
+		addResourceHandler("./site/js/", "/js", true);
+	}
+	
+	/**
+	 * 
+	 * @param pResourceBase
+	 * @param pContext
+	 * @param pListDirectories
+	 */
+	private void addResourceHandler(String pResourceBase, String pContext, boolean pListDirectories)
+	{
+		ResourceHandler rh = new ResourceHandler();
+		rh.setDirectoriesListed(pListDirectories);
+		rh.setResourceBase(pResourceBase);
+		
+		ContextHandler context = new ContextHandler(_contexts, pContext);
+		context.setHandler(rh);
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public void start() throws Exception
+	{
+		_server.start();
+		_server.join();
 	}
 	/*
 	 * FOR TESTING/DEBUGGING
